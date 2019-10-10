551d1bca5a9762c6f55662b6ed491eeffd6de94c
Jonathan Derrough <jonathan.derrough@gmail.com>
2019-10-10 16:17:50 +0200

    Cleaned up globalTeardown

--- tests/jest/globalTeardown.ts
+++ tests/jest/globalTeardown.ts
@@ -1,8 +1,8 @@
 module.exports = async (): Promise<void> => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const gbl: any = global;
 
     await gbl.httpServer.close();
-
     await gbl.photon.disconnect();
 
     console.log('Server shutdown');
