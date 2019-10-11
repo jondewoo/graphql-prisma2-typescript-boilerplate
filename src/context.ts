e8fff860229e8d2f89b882983249dd3b29e97675
Jonathan Derrough <jonathan.derrough@gmail.com>
2019-10-11 15:50:33 +0200

    Added pubsub

--- src/context.ts
+++ src/context.ts
@@ -1,13 +1,17 @@
 import { Context, ContextParameters } from 'graphql-yoga/dist/types';
+import { PubSub } from 'graphql-yoga';
 import { Photon } from '@generated/photon';
 import { photon } from './photon';
+import { pubsub } from './pubsub';
 
 export type Context = {
     photon: Photon;
+    pubsub: PubSub;
     ctxParams: ContextParameters;
 };
 
 export const createContext = (ctxParams: ContextParameters): Context => ({
     photon,
+    pubsub,
     ctxParams,
 });
